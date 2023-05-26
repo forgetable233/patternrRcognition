edd6a81031a1d5b42a8def663c4c67873f2fa16d
dcr <1456882404@qq.com>
2023-05-27 02:30:54 +0800

    finish data loader

--- data_loader.py
+++ data_loader.py
@@ -2,14 +2,34 @@ import numpy as np
 import struct
 import matplotlib.pyplot as plt
 import json
+import os
+import cv2
+
 
 class DataLoader:
     def __init__(self):
-        self.bmp_path = 'data/bmp/'
-        self.json_path = 'data/json/'
+        self.bmp = []
+        self.json = []
 
-        self.bmp = None
-        self.json = None
+        self.test_bmp = []
+        self.test_json = []
 
-        self.test_bmp = None
-        self.test_json = None
+    def load_data(self):
+        print('begin to load data\n')
+        current_address = os.path.dirname(os.path.abspath(__file__))
+        json_path = '/data/json/'
+        bmp_path = '/data/bmp/'
+        for roots, dirs, files in os.walk(current_address + json_path):
+            for file in files:
+                with open(current_address + json_path + file) as json_file:
+                    data = json.load(json_file)
+                    self.json.append(data)
+
+        for roots, dirs, files in os.walk(current_address + bmp_path):
+            for file in files:
+                img = cv2.imread(current_address + bmp_path + file)
+                self.bmp.append(img)
+
+        assert len(self.json) == len(self.bmp)
+        print('have successfully loaded data')
+        print('the input data size is ' + str(len(self.json)))
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from data_loader import DataLoader
 
 if __name__ == '__main__':
     print('hello world')
+    data = DataLoader()
+    data.load_data()
